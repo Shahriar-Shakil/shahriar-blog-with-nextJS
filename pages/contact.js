e993736ff7fb8a81750e4135b2619ece81086b39
mrshahriar <toshahriarshakil@gmail.com>
2022-11-10 21:38:35 +0600

    add Meta tag

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
+import { Fragment } from "react";
 import ContactForm from "../components/contact/contact-form";
 
 export default function Contact() {
-  return <ContactForm />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Me</title>
+        <meta name="description" content="Send me your message!" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+import { Fragment } from "react";
 import FeaturedPosts from "../components/homepage/featured-posts";
 import Hero from "../components/homepage/hero";
 import { getFeaturedPosts } from "../lib/posts-utils";
@@ -19,10 +21,17 @@ const DUMMY_POSTS = [
 ];
 function HomePage(props) {
   return (
-    <div>
+    <Fragment>
+      <Head>
+        <title>Shahriar' Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
-    </div>
+    </Fragment>
   );
 }
 export function getStaticProps() {
